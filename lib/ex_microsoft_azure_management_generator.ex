ece18d528112d43551877208c97163f3bbf3e365
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-05-05 17:05:51 +0200

    Cleanup

--- lib/ex_microsoft_azure_management_generator.ex
+++ lib/ex_microsoft_azure_management_generator.ex
@@ -4,8 +4,7 @@ defmodule ExMicrosoftAzureManagementGenerator do
 
   @java "java"
   @jar Application.get_env(:ex_microsoft_azure_management_generator, :jar_version)
-  @jar_source Application.get_env(:ex_microsoft_azure_management_generator, :jar_source)
-              |> String.to_charlist()
+  @jar_source Application.get_env(:ex_microsoft_azure_management_generator, :jar_source) |> String.to_charlist()
 
   @baseUrl "https://raw.githubusercontent.com/Azure/azure-rest-api-specs"
   @branch "master"
@@ -22,8 +21,9 @@ defmodule ExMicrosoftAzureManagementGenerator do
     end
   end
 
-  def generate([]), do: :ok
-  def generate([api = %API{} | tail]) do
+  defp generate(api = %API{}) do
+    init()
+
     IO.puts("Writing package=#{api.package} name=#{api.name}")
 
     generatorConfig =
@@ -33,7 +33,9 @@ defmodule ExMicrosoftAzureManagementGenerator do
     configFileName = "#{api.name}.json"
     configFileName |> File.write!(generatorConfig)
 
-    args = "-jar #{@jar} generate -l elixir -i #{api.url} -o clients/#{api.package} -c #{configFileName}"
+    args = "-jar #{@jar} generate -l elixir " <>
+      "-i #{api.url} -o clients/#{api.package} -c #{configFileName}"
+
     IO.puts("Running #{@java} #{args}")
 
     {_stdout, 0} = System.cmd(@java,
@@ -42,9 +44,7 @@ defmodule ExMicrosoftAzureManagementGenerator do
         cd: Path.absname(".")
       )
 
-    # configFileName |> File.rm!()
-
-    generate(tail)
+    configFileName |> File.rm!()
   end
 
   defp parse_config_file(filename) do
@@ -57,7 +57,7 @@ defmodule ExMicrosoftAzureManagementGenerator do
   def generate_from_json() do
     "swagger.json"
     |> parse_config_file()
-    |> generate()
+    |> Enum.each(&generate/1)
   end
 
   defp tweak_name(name) do
@@ -68,31 +68,9 @@ defmodule ExMicrosoftAzureManagementGenerator do
     |> String.replace("PowerBIdedicated", "PowerBiDedicated")
   end
 
-  defp to_api(l = [_pref, "resource-management", api_version, _file]),
-    do: %API{
-      package: "Microsoft.Azure.Management",
-      url: @url_pref <> Enum.join(l, "/"),
-      apiVersion: api_version
-    }
-
-  defp to_api(l = [_pref, "resource-manager", name, state, api_version, _file]),
-    do: %API{
-      package: "Microsoft.Azure.Management",
-      name: name |> tweak_name(),
-      url: @url_pref <> Enum.join(l, "/"),
-      state: state,
-      apiVersion: api_version
-    }
-
-  defp to_api(l = [_pref, "resource-manager", name, state, api_version, _area, _file]),
-    do: %API{
-      package: "Microsoft.Azure.Management",
-      name: name |> tweak_name(),
-      url: @url_pref <> Enum.join(l, "/"),
-      state: state,
-      apiVersion: api_version
-    }
-
+  defp to_api(l = [_pref, "resource-management", api_version, _file]), do: %API{ package: "Microsoft.Azure.Management", url: @url_pref <> Enum.join(l, "/"), apiVersion: api_version}
+  defp to_api(l = [_pref, "resource-manager", name, state, api_version, _file]), do: %API{ package: "Microsoft.Azure.Management", name: name |> tweak_name(), url: @url_pref <> Enum.join(l, "/"), state: state, apiVersion: api_version }
+  defp to_api(l = [_pref, "resource-manager", name, state, api_version, _area, _file]), do: %API{ package: "Microsoft.Azure.Management", name: name |> tweak_name(), url: @url_pref <> Enum.join(l, "/"), state: state, apiVersion: api_version }
   defp to_api(["azsadmin", "resource-manager", _area, _name, _state, _api_version, _file]), do: nil
   defp to_api([_pref, "control-plane", _name, _state, _api_version, _file]), do: nil
   defp to_api([_pref, "data-plane", _name, _area, _state, _api_version, _file]), do: nil
@@ -112,11 +90,11 @@ defmodule ExMicrosoftAzureManagementGenerator do
     |> File.stream!([:read, :utf8], :line)
     |> Stream.map(&parse_url/1)
     |> Stream.filter(&(&1 != nil))
-    |> Enum.map(& &1)
-    |> generate()
+    # |> Enum.map(& &1)
+    |> Enum.each(&generate/1)
 
     IO.puts("Fixing body handling (https://github.com/swagger-api/swagger-codegen/issues/8138)")
-    System.cmd("fix_body.sh", [], stderr_to_stdout: true, cd: Path.absname("clients"))
+    System.cmd("sh", ["fix_body.sh"], stderr_to_stdout: true, cd: Path.absname("clients"))
   end
 
   def generate_from_text_file(), do: "swagger.txt" |> Path.absname() |> generate_from_text_file()
