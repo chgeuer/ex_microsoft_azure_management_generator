91cddac32d2be2ef38b58a3df315ed73eecabe9f
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-05-06 19:51:40 +0200

    Fix all problems with body content

--- fix_body.sh
+++ fix_body.sh
@@ -2,14 +2,8 @@
 
 echo "Fixing body handling (https://github.com/swagger-api/swagger-codegen/issues/8138)"
 
-find clients \
+find 1 \
    -type f \
    -name "*.ex" \
    -exec sed -i'' \
-    -e 's/add_param(:body, :"parameters", parameters)/add_param(:body, :body, parameters)/g' {} +
-
-find clients \
-   -type f \
-   -name "*.ex" \
-   -exec sed -i'' \
-    -e 's/add_param(:body, :"createUpdateParameters", create_update_parameters)/add_param(:body, :body, create_update_parameters)/g' {} +
+    -e 's/add_param(:body, :"[^"]*", /add_param(:body, :body, /g' {} +
